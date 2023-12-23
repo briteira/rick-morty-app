a32cd54326a6e566c63f20116537aadd09233b1c
Gustavo Brito <brito-dev@hotmail.com>
2023-12-23 20:04:20 -0300

    Utilizacao dos arquivos de traducao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MenuSidebarComponent } from './components/menu-sidebar/menu-sidebar.component';
-import { TranslateModule } from '@ngx-translate/core';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -18,7 +25,15 @@ import { TranslateModule } from '@ngx-translate/core';
     BrowserModule,
     CommonModule,
     RouterModule,
-    TranslateModule.forRoot()
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      defaultLanguage: 'pt'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
