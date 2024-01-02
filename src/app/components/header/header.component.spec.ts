d7943c6810e0f6574054d235b9e32a6bfe5c9aed
Gustavo Brito <brito-dev@hotmail.com>
2024-01-02 20:30:37 -0300

    Adicionar testes unitarios - header e inputsearch

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,18 +1,20 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
 
 import { HeaderComponent } from './header.component';
-import { TranslateModule } from '@ngx-translate/core';
-import { BsModalService } from 'ngx-bootstrap/modal';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { MatIconModule } from '@angular/material/icon';
+import { usersMock } from 'src/app/mocks/users.mock';
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let modalService: BsModalService;
 
   beforeEach(async(() => {
+
     TestBed.configureTestingModule({
       declarations: [ HeaderComponent ],
       imports: [
@@ -20,10 +22,12 @@ describe('HeaderComponent', () => {
         MatIconModule
       ],
       providers: [
-        BsModalService
+        BsModalService,
       ]
     })
     .compileComponents();
+
+    modalService = TestBed.inject(BsModalService);
   }));
 
   beforeEach(() => {
@@ -34,5 +38,53 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve abrir o modal de confirmação ao chamar openModalLogout', () => {
+    const template = {} as any;
+
+    const showSpy = spyOn(modalService, 'show').and.returnValue({
+      content: {},
+      hide: () => {},
+    } as BsModalRef);
+
+    component.userAuthenticated = usersMock[0];
+
+    component.openModalLogout(template);
+
+    expect(showSpy).toHaveBeenCalledWith(template, { class: 'modal-sm' });
+  });
+
+  it('deve fechar o modal de confirmação ao cancelar', () => {
+    const template = {} as any;
+
+    const modalRef = {
+      content: {},
+      hide: jasmine.createSpy('hide'),
+    } as unknown as BsModalRef;
+
+    const showSpy = spyOn(modalService, 'show').and.returnValue(modalRef);
+
+    component.userAuthenticated = usersMock[0];
+
+    component.openModalLogout(template);
+
+    component.decline();
+
+    expect(modalRef.hide).toHaveBeenCalled();    
+  })
+
+  it('deve exibir o botao para logout apenas quando usuario estiver logado', () => {
+    component.userAuthenticated = usersMock[0];
+    fixture.detectChanges();
+
+    const logoutButton = fixture.debugElement.query(By.css('#btn-abrir-modal-logout'));
+    expect(logoutButton).toBeTruthy();
+
+    component.userAuthenticated = null;
+    fixture.detectChanges();
+
+    const logoutButtonAfterLogout = fixture.debugElement.query(By.css('#btn-abrir-modal-logout'));
+    expect(logoutButtonAfterLogout).toBeNull();
   });
 });
--- src/app/components/input-search/input-search.component.spec.ts
+++ src/app/components/input-search/input-search.component.spec.ts
@@ -7,10 +7,12 @@ import { InputSearchComponent } from './input-search.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatIconModule } from '@angular/material/icon';
 import { ReactiveFormsModule } from '@angular/forms';
+import { InputSearchService } from './input-search.service';
 
 describe('InputSearchComponent', () => {
   let component: InputSearchComponent;
   let fixture: ComponentFixture<InputSearchComponent>;
+  let inputSearchService: InputSearchService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -19,9 +21,14 @@ describe('InputSearchComponent', () => {
         TranslateModule.forRoot(),
         MatIconModule,
         ReactiveFormsModule
+      ],
+      providers: [
+        InputSearchService
       ]
     })
     .compileComponents();
+
+    inputSearchService = TestBed.inject(InputSearchService);
   }));
 
   beforeEach(() => {
@@ -32,5 +39,21 @@ describe('InputSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve atualizar o valor do formGroup ao alterar o nome no serviço nome$', () => {
+    const mockNome = 'Nome de Teste';
+    inputSearchService.setNome(mockNome);
+    expect(component.formGroup.get('nome')?.value).toEqual(mockNome);
+  });
+
+  it('deve emitir o evento aoBuscar e chamar setNome ao buscar', () => {
+    spyOn(component.aoBuscar, 'emit');
+    const mockNome = 'Nome de Teste';
+    component.formGroup.get('nome')?.setValue(mockNome);
+    fixture.detectChanges();
+    component.buscar();
+
+    expect(component.aoBuscar.emit).toHaveBeenCalledWith(mockNome);
   });
 });
