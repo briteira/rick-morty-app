1fa9d7d32b4d3c5340a3cd6b323323dc551ad61e
Gustavo Brito <brito-dev@hotmail.com>
2024-01-02 21:25:12 -0300

    Adicionar testes unitarios - character list

--- src/app/modules/character/character-list/character-list.component.spec.ts
+++ src/app/modules/character/character-list/character-list.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement, Injectable } from '@angular/core';
 
@@ -12,12 +12,20 @@ import { InputSearchComponent } from 'src/app/components/input-search/input-sear
 import { MatIconModule } from '@angular/material/icon';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RickMortyService } from 'src/app/services/rick-morty.service';
+import { RickMortyResponseModel } from 'src/app/models/rick-morty-response.model';
+import { CharacterModel } from 'src/app/models/character.model';
+import { of } from 'rxjs';
 
 describe('CharacterListComponent', () => {
   let component: CharacterListComponent;
   let fixture: ComponentFixture<CharacterListComponent>;
+  let rickMortyServiceSpy: jasmine.SpyObj<RickMortyService>;
 
   beforeEach(async(() => {
+
+    rickMortyServiceSpy = jasmine.createSpyObj('RickMortyService', ['getCharacters']);
+
     TestBed.configureTestingModule({
       declarations: [ CharacterListComponent, InputSearchComponent ],
       imports: [ 
@@ -29,7 +37,8 @@ describe('CharacterListComponent', () => {
         TooltipModule
       ],
       providers: [
-        BsModalService
+        BsModalService,
+        { provide: RickMortyService, useValue: rickMortyServiceSpy },
       ]
     })
     .compileComponents();
@@ -44,4 +53,41 @@ describe('CharacterListComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+   it('deve chamar carregarDados durante a inicialização', () => {
+    const mockResponse: RickMortyResponseModel<CharacterModel> = {
+      results: [],
+      info: { count: 0, pages: 1, next: '', prev: '' },
+    };
+  
+    rickMortyServiceSpy.getCharacters.and.returnValue(of(mockResponse));
+    
+    fixture.detectChanges();
+  
+    expect(rickMortyServiceSpy.getCharacters).toHaveBeenCalled();
+  });
+
+  it('deve ajustar a altura da tabela corretamente após ngAfterViewInit', fakeAsync(() => {
+    spyOn(component, 'ajustarAlturaTabela');
+    
+    fixture.detectChanges();
+    tick();
+
+    component.ngAfterViewInit();
+
+    expect(component.ajustarAlturaTabela).toHaveBeenCalled();
+  }));
+
+  it('deve atualizar filtros e chamar carregarDados ao chamar aoBuscarInputSearch', fakeAsync(() => {
+    spyOn(component, 'scrollToTop');
+    spyOn(component, 'carregarDados');
+
+    component.aoBuscarInputSearch('Rick');
+
+    expect(component.scrollToTop).toHaveBeenCalled();
+    expect(component.pagina).toBe(1);
+    expect(component.filtroNome).toBe('Rick');
+    expect(component.dadosTotalCarregado).toBeFalse();
+    expect(component.carregarDados).toHaveBeenCalled();
+  }));
 });
